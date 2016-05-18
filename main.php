d630b2c26c76db3e664172ace127ea39046ad085
Tanvir Chahal <tnvr009@gmail.com>
2016-05-18 15:55:48 +0530

    Revert "Revert "Main File Imported""
    This reverts commit a40b997658bca3db8ffc7b5ad6e1f88391e5565c.

--- main.php
+++ main.php
@@ -1,8 +1,12 @@
 <?php 
+
+   // Receiving the arguments on the server task
    $email = stripslashes($argv[1]);
    $password = stripslashes($argv[2]);
    
    $handle = fopen("test.txt", "w");
+   
+   //writing a file called test.txt, contains custom passed data
    fwrite($handle, "hi ".$email." ".$password);
    
 
